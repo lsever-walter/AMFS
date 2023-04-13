67f4f0f9b918abf85b0a45319b6ee69ca6b909a4
lsever-walter <100168038+lsever-walter@users.noreply.github.com>
2023-04-13 15:46:11 -0700

    Update plotter_applet.py

--- Controller/plotter_applet.py
+++ Controller/plotter_applet.py
@@ -12,20 +12,17 @@ from artiq.applets.simple import SimpleApplet
 from jax import RealTimePlotApplet, JaxApplet
 
 
-class PlotPMT(RealTimePlotApplet):
+class PlotVoltageData(RealTimePlotApplet):
     def __init__(self, args, **kwds):
         num_of_traces = 1  
-        datasets_names = ["times", "voltages"]
+        datasets_names = ["voltage.trace_data"]
         xlabel = "Time (s)"
         ylabel = "Voltage (V)"
         super().__init__(num_of_traces, datasets_names, xlabel, ylabel, **kwds)
 
     def _set(self, dataset_name, value):
-        if dataset_name == "times":
-            self._times = value
-        elif dataset_name == "voltages":
-            for kk in range(len(value)):
-                self.traces.set(self._times, value[kk])
+        if dataset_name == "voltage.trace_data":
+            self._trace_data = value
 
     def _append(self, dataset_name, value):
         if dataset_name == "pmt.times":
@@ -33,13 +30,13 @@ class PlotPMT(RealTimePlotApplet):
                 self.traces[kk].append_x(value)
         elif dataset_name == "pmt.counts_kHz":
             for kk in range(len(self.traces)):
-                self.traces[kk].append_y(value[kk])
-                self.traces[kk].update_trace()  # only update the plot when counts are updated.
+                self.traces[0].append_y(value)
+                self.traces[0].update_trace()  # only update the plot when counts are updated.
 
 
 def main():
-    applet = SimpleApplet(PlotPMT)
-    PlotPMT.add_labrad_ip_argument(applet)
+    applet = SimpleApplet(PlotVoltageData)
+    PlotVoltageData.add_labrad_ip_argument(applet)
     applet.run()
 
 
