bc6cbc9b27b4647f2471897c8a7543074796e429
lsever-walter <100168038+lsever-walter@users.noreply.github.com>
2023-05-13 10:52:43 -0700

    filter graphs

--- Controller/data_analysis.py
+++ Controller/data_analysis.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load the data from the CSV file
-data = np.genfromtxt('/Users/lseverwalter/code/AMFS/Controller/Data/CSV/BField_Noise_3600_1.csv', delimiter=',')
+data = np.genfromtxt('/Users/lseverwalter/code/AMFS/Controller/Data/Active_Filter/no_filter_voltage.csv', delimiter=',')
 # Split the columns into separate numpy arrays
 
 t0 = np.array(data[:, 0])
 v0 = np.array(data[:, 1])
-# v1 = np.array(data[:, 2])
+v1 = np.array(data[:, 2])
 
 
 def absFFT(times,amplitude):
@@ -31,7 +31,7 @@ def plotFourrier(t0, v0, t1=0, v1=0):
     ax.plot(freq0, fft0)
     ax.set_xlabel("frequency [Hz]")
     ax.set_ylabel("Amplitude")
-    ax.set_xlim((min(freq0), 2500))
+    ax.set_xlim((min(freq0), 500))
     ax.set_ylim((min(fft0)-.00005, .00005+max(fft0)))
 
     ax.xaxis.set_major_locator(plt.MaxNLocator(5))
@@ -39,14 +39,15 @@ def plotFourrier(t0, v0, t1=0, v1=0):
     #ax.xaxis.set_minor_locator(plt.FixedLocator(np.arange(ax.get_xlim()[0], ax.get_xlim()[1], 100)))
 
     # Add major and minor ticks to y-axis
-    ax.yaxis.set_major_locator(plt.MaxNLocator(5))
-    ax.yaxis.set_minor_locator(plt.MaxNLocator(25))
-    #ax.yaxis.set_minor_locator(plt.FixedLocator(np.arange(ax.get_ylim()[0], ax.get_ylim()[1], .00005)))
+    ax.yaxis.set_major_locator(plt.MaxNLocator(4))
+    ax.yaxis.set_minor_locator(plt.MaxNLocator(24))
+    ax.yaxis.set_minor_locator(plt.FixedLocator(np.arange(ax.get_ylim()[0], ax.get_ylim()[1], .00005)))
+
 
     ax.tick_params(direction='in', top=True, right=True, which='both')
 
     #write code that adds major and minor ticks that are oriented inwards on all axes and that sets x and y limits adjusted for the maximum and minimum values for the y and x data
-    plt.savefig("/Users/lseverwalter/code/AMFS/Controller/Data/FFT_Plots/FFT_BField_Noise_5_1", dpi=300, transparent=False)
+    #plt.savefig("/Users/lseverwalter/code/AMFS/Controller/Data/Active_Filter/active_filter_voltage_7", dpi=300, transparent=False)
     plt.show()
 
 
@@ -70,20 +71,37 @@ def plot_data(t0, v0, t1=0, v1=0):
 
     ax.tick_params(direction='in', top=True, right=True, which='both')
 
-    plt.savefig("/Users/lseverwalter/code/AMFS/Controller/Data/Current_Plots/Coil_BField_5_1", dpi=300, transparent=False)
+    # plt.savefig("/Users/lseverwalter/code/AMFS/Controller/Data/Active_Filter/active_filter_voltage_7", dpi=300, transparent=False)
     plt.show()
 
 
 def drift_quantify(v0):
     v_std = np.std(v0)
     v_rms = np.sqrt(np.mean(v0**2))
-
+    print(f"The standard deviation is: {v_rms}")
+    print(f"The rms is: {v_rms}")
     return v_std, v_rms
 
 
+def find_max(t0, v0):
+    freq0, fft0 = absFFT(t0,v0)
+    freq0 = freq0[10:]
+    fft0 = fft0[10:]
+    sorted_indices = np.argsort(fft0)[-3:]
+    sorted_values = fft0[sorted_indices]
+    print(f"The max amplitudes are {sorted_values}")
+    print(f"The values at which they occur at: {sorted_values}")
 
-# plot_data(t0, v0)
-# plotFourrier(t0, v0)
-std, rms = drift_quantify(v0)
-print(std, rms)
+
+    return sorted_indices, sorted_values
+
+
+
+
+plot_data(t0, v0)
+plotFourrier(t0, v0)
+# std, rms = drift_quantify(v0)
+find_max(t0,v0)
+
+
 
