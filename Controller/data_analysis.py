fde1d76cc8d05286efd9c213d9d098867b9dbebb
lsever-walter <100168038+lsever-walter@users.noreply.github.com>
2023-05-14 18:14:04 -0700

    refactoring

--- Controller/data_analysis.py
+++ Controller/data_analysis.py
@@ -4,12 +4,18 @@ import matplotlib.ticker as tck
 from scipy import integrate
 
 # Load the data from the CSV file
-data = np.genfromtxt('/Users/lseverwalter/code/AMFS/Controller/Data/Active_Filter/no_filter_voltage.csv', delimiter=',')
+data0 = np.genfromtxt('/Users/lseverwalter/code/AMFS/Controller/Data/Active_Filter/CSV/no_filter_voltage.csv', delimiter=',')
+data1 = np.genfromtxt('/Users/lseverwalter/code/AMFS/Controller/Data/Active_Filter/CSV/active_filter_voltage_8.csv', delimiter=',')
 # Split the columns into separate numpy arrays
 
-t0 = np.array(data[:, 0])
-v0 = np.array(data[:, 1])
-v1 = np.array(data[:, 2])
+t0 = np.array(data0[:, 0])
+v0 = np.array(data0[:, 1])
+# v01 = np.array(data0[:, 2])
+
+t1 = np.array(data1[:, 0])
+v1 = np.array(data1[:, 1])
+# v11 = np.array(data1[:, 2])
+
 
 
 def absFFT(times,amplitude):
@@ -22,21 +28,53 @@ def absFFT(times,amplitude):
     frequencies = values/timePeriod
     return frequencies, abs(fourierTransform)
 
+def drift_quantify(v0):
+    v_std = np.std(v0)
+    v_rms = np.sqrt(np.mean(v0**2))
+    print(f"The standard deviation is: {v_rms}")
+    print(f"The rms is: {v_rms}")
+    return v_std, v_rms
 
-def plotFourrier(t0, v0, t1=0, v1=0, log=False):
+
+def integrator(t0, v0):
     freq0, fft0 = absFFT(t0,v0)
-    
-    area = noise_quantify(t0, v0)
+    area_simpson = integrate.simpson(y=fft0, x=freq0)
+    return round(area_simpson, 6)
 
-    freq0 = freq0[1:]
-    fft0 = fft0[1:]
-
+def plot_data(t, v):
     fig, ax = plt.subplots(1, 1, num=3)
-    ax.plot(freq0, fft0)
+    ax.plot(t, v)
+    ax.set_xlabel("Time [s]")
+    ax.set_ylabel("Voltage [V]")
+    ax.set_xlim((-.25+min(t), .25+max(t)))
+    ax.set_ylim((min(v)-.005, .005+max(v)))
+
+    ax.legend(loc='upper left', fontsize=16)
+    ax.tick_params(axis='both', which='both', direction='in',
+                        top=True, right=True, labelsize=16)
+    ax.xaxis.set_minor_locator(tck.AutoMinorLocator())
+    ax.yaxis.set_minor_locator(tck.AutoMinorLocator())
+    ax.xaxis.label.set_size(20)
+    ax.yaxis.label.set_size(20)
+    
+    #plt.savefig("/Users/lseverwalter/code/AMFS/Controller/Data/Active_Filter/Raw/no_filter_raw", dpi=300, transparent=False)
+    plt.show()
+
+
+def plotFourrier(t, v, log=False):
+    freq, fft = absFFT(t,v)
+    
+    area = round(integrator(t, v), 6)
+
+    freq = freq[1:]
+    fft = fft[1:]
+
+    fig, ax = plt.subplots(1, 1, num=1)
+    ax.plot(freq, fft, label=f"Area= {area}")
     ax.set_xlabel("frequency [Hz]")
     ax.set_ylabel("Amplitude")
-    ax.set_xlim((min(freq0), 500))
-    ax.set_ylim((min(fft0)-.00005, .00005+max(fft0)))
+    ax.set_xlim((min(freq), 1000))
+    ax.set_ylim((min(fft)-(4e-6), .00005+max(fft)))
 
     ax.legend(loc='upper left', fontsize=16)
     ax.tick_params(axis='both', which='both', direction='in',
@@ -49,25 +87,40 @@ def plotFourrier(t0, v0, t1=0, v1=0, log=False):
     if log:
         ax.set_xscale('log')
         ax.set_yscale('log')
-        ax.set(xlabel='Log(Frequency)')
+        ax.set(xlabel='Log(Frequency)', xlim=None)
         ax.set(ylabel='Log(Power)', ylim=(5e-9, 1))
+        
+        
+        
+        #plt.savefig("/Users/lseverwalter/code/AMFS/Controller/Data/Active_Filter/FFT_LL/no_filter_fft_ll", dpi=300, transparent=False)
 
     elif not log:
+        #plt.savefig("/Users/lseverwalter/code/AMFS/Controller/Data/Active_Filter/FFT/no_filter_fft", dpi=300, transparent=False)
         pass
-
-    #write code that adds major and minor ticks that are oriented inwards on all axes and that sets x and y limits adjusted for the maximum and minimum values for the y and x data
-    #plt.savefig("/Users/lseverwalter/code/AMFS/Controller/Data/Active_Filter/active_filter_voltage_7", dpi=300, transparent=False)
+    
+    
     plt.show()
 
 
+def overplot_fourrier(t0, v0, t1, v1, log=False):
+    freq0, fft0 = absFFT(t0,v0)
+    freq1, fft1 = absFFT(t1, v1)
 
-def plot_data(t0, v0, t1=0, v1=0):
+    area0 = round(integrator(t0, v0), 6)
+    area1 = round(integrator(t1, v1), 6)
+
+    freq0 = freq0[2:]
+    fft0 = fft0[2:]
+    freq1 = freq1[2:]
+    fft1 = fft1[2:]
+
     fig, ax = plt.subplots(1, 1, num=2)
-    ax.plot(t0, v0)
-    ax.set_xlabel("Time [s]")
-    ax.set_ylabel("Voltage [V]")
-    ax.set_xlim((-.25+min(t0), .25+max(t0)))
-    ax.set_ylim((min(v0)-.005, .005+max(v0)))
+    ax.plot(freq0, fft0, label=f"Area= {area0}")
+    ax.plot(freq1, fft1, label=f"Area= {area1}")
+    ax.set_xlabel("frequency [Hz]")
+    ax.set_ylabel("Amplitude")
+    ax.set_xlim((min(freq0)-30, 1000))
+    ax.set_ylim((min(fft0)-(4e-6), .00005+max(fft0)))
 
     ax.legend(loc='upper left', fontsize=16)
     ax.tick_params(axis='both', which='both', direction='in',
@@ -76,30 +129,28 @@ def plot_data(t0, v0, t1=0, v1=0):
     ax.yaxis.set_minor_locator(tck.AutoMinorLocator())
     ax.xaxis.label.set_size(20)
     ax.yaxis.label.set_size(20)
-    
-    # plt.savefig("/Users/lseverwalter/code/AMFS/Controller/Data/Active_Filter/active_filter_voltage_7", dpi=300, transparent=False)
+
+    if log:
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.set(xlabel='Log(Frequency)', xlim=None)
+        ax.set(ylabel='Log(Power)', ylim=(5e-9, 1))
+        
+    elif not log:
+        pass
+   
+    #plt.savefig("/Users/lseverwalter/code/AMFS/Controller/Data/Active_Filter/FFT_Overplot/no_filter_filter_8", dpi=300, transparent=False)
     plt.show()
 
 
-def drift_quantify(v0):
-    v_std = np.std(v0)
-    v_rms = np.sqrt(np.mean(v0**2))
-    print(f"The standard deviation is: {v_rms}")
-    print(f"The rms is: {v_rms}")
-    return v_std, v_rms
-
-
-def noise_quantify(t0, v0):
-    freq0, fft0 = absFFT(t0,v0)
-    area_simpson = integrate.simpson(y=fft0, x=freq0)
-    return area_simpson,
 
 
 
 plot_data(t0, v0)
-plotFourrier(t0, v0, log=True)
-# std, rms = drift_quantify(v0)
-noise_quantify(t0, v0)
+plotFourrier(t1, v1, log=False)
+# plotFourrier(t0, v0, log=False)
+overplot_fourrier(t0, v0, t1, v1, log=True)
+
 
 
 
